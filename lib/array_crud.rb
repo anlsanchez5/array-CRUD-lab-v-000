2357cadd27cadc81dd04cbd8b79f199583f358ab
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-09-06 21:25:34 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  bible_books = [Romans, Psalms, Ephesians, Philippians]
+  bible_books = ["Romans", "Psalms", "Ephesians", "Philippians"]
 end
 
 def add_element_to_end_of_array(array, element)
